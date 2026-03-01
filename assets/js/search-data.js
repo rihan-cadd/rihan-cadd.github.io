8a0cd0a5b9a143b2fa7def97b80a3b13c7bc1cf9
rihan-cadd <ri.han.cadd@gmail.com>
2026-03-01 18:52:26 +0000

    Deploying to gh-pages from @ rihan-cadd/rihan-cadd.github.io@35a2cda2b472e053412f50662233c839705592fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-cite-start-honored-to-receive-the-best-poster-award-2nd-class-at-the-asia-hub-for-e-drug-discovery-symposium-2025-ahedd2025-cite-94-95-cite-start-the-award-was-presented-for-the-work-on-metabognn-predicting-liver-metabolic-stability-using-gnns-and-cross-species-data-cite-99-102",
+          title: '[cite_start]Honored to receive the Best Poster Award (2nd Class) at the Asia Hub...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
